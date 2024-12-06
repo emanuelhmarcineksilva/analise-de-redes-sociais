587a15d5f35ada91b73bf6fcd217e525e319929e
Emanuel <105359101+emanuelhmarcineksilva@users.noreply.github.com>
2024-12-06 16:44:00 -0300

    Add files via upload

---
+++ projetos.js
@@ -0,0 +1,11 @@
+const pgProjetos = document.querySelector('.pagina-projetos')
+
+function surgirPgFlutuante() {
+    pgProjetos.style.display = "flex"
+}
+
+function esconderPgFlutuante() {
+    pgProjetos.style.display = "none"
+}
+
+
